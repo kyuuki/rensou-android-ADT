291d882336bd726dc792bcbd3eb2c872b333b9e0
kyuuki <kyuuki0@gmail.com>
2013-09-06 20:40:42 +0900

    AdMob 導入。

--- src/jp/kyuuki/rensou/android/activity/MainActivity.java
+++ src/jp/kyuuki/rensou/android/activity/MainActivity.java
@@ -1,13 +1,29 @@
 package jp.kyuuki.rensou.android.activity;
 
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+
 import jp.kyuuki.rensou.android.R;
 import android.os.Bundle;
+import android.view.WindowManager.LayoutParams;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // 自動でソフトキーボードが出るのを防ぐ
+        // http://y-anz-m.blogspot.jp/2010/05/android_17.html
+        getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); 
+        
         setContentView(R.layout.activity_main);
+        
+        // AdView をリソースとしてルックアップしてリクエストを読み込む。
+        // https://developers.google.com/mobile-ads-sdk/docs/android/banner_xml?hl=ja
+        AdView adView = (AdView)this.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest();
+        //adRequest.addTestDevice("6E5BC489C3B529363F063C3C74151BC7");
+        adView.loadAd(adRequest);
     }
 }
---
+++ src/jp/kyuuki/rensou/android/common/RensouUtils.java
@@ -0,0 +1,6 @@
+package jp.kyuuki.rensou.android.common;
+
+public final class RensouUtils {
+
+    private RensouUtils() {}
+}
